1b629a56059fe395e2c68670df053fee23509bf9
Anirudhwa Ankon <ankon495@gmail.com>
2022-11-24 00:16:02 +0600

    Finalized how to have a modern modal system.

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+
+const btnsShowModal = document.querySelectorAll(`.show-modal`);
+
+const modalClose = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
+const modalOpen = function () {
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener(`click`, modalOpen);
+
+btnCloseModal.addEventListener(`click`, modalClose);
+
+overlay.addEventListener(`click`, modalClose);
+
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `Escape` && !modal.classList.contains(`hidden`)) {
+    modalClose();
+  }
+});
